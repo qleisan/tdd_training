cecbc227747072ba8441a76e57074ba09fac39b7
Leif Sandström <leif.sandstrom@semcon.com>
2022-06-18 20:29:49 +0200

    Calculator Step 2,3

--- StringCalc.py
+++ StringCalc.py
@@ -1,10 +1,19 @@
+import re
+
 class StringCalc():
     def add(self, numbers):
         if numbers == "":
             return 0
-        number_list = numbers.split(",")
+        separators = r",|\n"
+        if numbers[0:2] == "//":
+            separator_end = numbers.index("\n")
+            separators += "|" + numbers[2:separator_end]
+            numbers = numbers[separator_end+1:]
+        number_list = re.split(separators, numbers)
         total = 0
         for number in number_list:
+            print("number =" + number)
             total += int(number)
         return total
+
 
--- test_StringCalc.py
+++ test_StringCalc.py
@@ -1,13 +1,22 @@
 from StringCalc import *
 
-def test_test_empty_string_returns_zero():
+def test_empty_string_returns_zero():
     assert StringCalc().add("") == 0
 
-def test_test_single_number_string_returns_value():
+def test_single_number_string_returns_value():
     assert StringCalc().add("1") == 1
 
-def test_test_two_number_string_returns_sum():
+def test_two_number_string_returns_sum():
     assert StringCalc().add("1,2") == 3
 
-def test_test_large_number_of_large_numbers_returns_sum():
+def test_large_number_of_large_numbers_returns_sum():
     assert StringCalc().add("100,53,-8,65,3541") == 3751
+
+def test_should_return_sum_when_numbers_are_separated_by_newline():
+    assert StringCalc().add("1\n2,3") == 6
+
+def test_should_return_sum_for_user_defined_separator():
+    assert StringCalc().add("//;\n1;2") == 3
+
+def test_should_return_sum_for_long_user_defined_separator():
+    assert StringCalc().add("//;;\n1;;2") == 3
