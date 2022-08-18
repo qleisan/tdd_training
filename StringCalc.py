6b9d2019c09fe6e35846b45457a6ef3132da59e7
Leif Sandström <leif.sandstrom@semcon.com>
2022-08-18 16:33:56 +0200

    Add Black badge and format files

--- StringCalc.py
+++ StringCalc.py
@@ -1,9 +1,11 @@
 import re
 
+
 class CustomException(Exception):
     pass
 
-class StringCalc():
+
+class StringCalc:
     def add(self, numbers):
         if numbers == "":
             return 0
@@ -11,7 +13,7 @@ class StringCalc():
         if numbers[0:2] == "//":
             separator_end = numbers.index("\n")
             separators += "|" + numbers[2:separator_end]
-            numbers = numbers[separator_end+1:]
+            numbers = numbers[separator_end + 1 :]
         number_list = re.split(separators, numbers)
         total = 0
         for number in number_list:
@@ -20,5 +22,3 @@ class StringCalc():
             print("number =" + number)
             total += int(number)
         return total
-
-
--- test_StringCalc.py
+++ test_StringCalc.py
@@ -1,43 +1,54 @@
 from StringCalc import *
 import pytest
 
+
 def test_empty_string_returns_zero():
     assert StringCalc().add("") == 0
+
 
 def test_single_number_string_returns_value():
     assert StringCalc().add("1") == 1
 
+
 def test_two_number_string_returns_sum():
     assert StringCalc().add("1,2") == 3
+
 
 def test_large_number_of_large_numbers_returns_sum():
     assert StringCalc().add("100,53,0,65,3541") == 3759
 
+
 def test_should_return_sum_when_numbers_are_separated_by_newline():
     assert StringCalc().add("1\n2,3") == 6
+
 
 def test_should_return_sum_for_user_defined_separator():
     assert StringCalc().add("//;\n1;2") == 3
 
+
 def test_should_return_sum_for_long_user_defined_separator():
     assert StringCalc().add("//;;\n1;;2") == 3
+
 
 def test_should_throw_exception_for_negative_numbers_1():
     # passes if Exception (or any subclass) is raised
     with pytest.raises(Exception):
         StringCalc().add("-1")
 
-#@pytest.mark.skip(reason="no reason")
+
+# @pytest.mark.skip(reason="no reason")
 def test_should_throw_exception_for_negative_numbers_2():
     # passes if Exception (or any subclass) is raised AND the message is correct
     with pytest.raises(Exception) as e:
         StringCalc().add("-1")
     assert str(e.value) == "Negatives not allowed: -1"
 
+
 def test_should_throw_exception_for_negative_numbers_3():
     # passes if CustomException (or any subclass) is raised
     with pytest.raises(CustomException):
         StringCalc().add("-1")
+
 
 def test_should_throw_exception_for_negative_numbers_4():
     # passes if CustomException (or any subclass) is raised AND the message is correct
